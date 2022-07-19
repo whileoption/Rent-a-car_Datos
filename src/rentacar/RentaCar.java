dc2ad839a1550bc0c0840a9b4f25da92656edb45
KarinaMadrigal <101362675+KarinaMadrigal@users.noreply.github.com>
2022-07-18 20:29:42 -0600

    Start

---
+++ src/rentacar/RentaCar.java
@@ -0,0 +1,18 @@
+/*
+ */
+package rentacar;
+
+/**
+ *
+ * @author 
+ */
+public class RentaCar {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
